5186fb20316ac8fcf861aa34b8f447944bd90e36
codinBabe <teboola2@gmail.com>
2024-08-07 07:12:33 +0100

    Fix: semistandard error

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -2,8 +2,10 @@ import { uploadPhoto, createUser } from "./utils.js";
 
 export default function handleProfileSignup() {
   return Promise.all([uploadPhoto(), createUser()])
-    .then(([value1, value2]) => {
-      console.log(value1.body, value2.firstName, value2.lastName);
+    .then((values) => {
+      console.log(
+        `${values[0].body}, ${values[1].firstName}, ${values[1].lastName}`
+      );
     })
     .catch(() => console.log("Signup system offline"));
 }
