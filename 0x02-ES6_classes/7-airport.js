84c32f364ecb590e25460954e85e3a175e4d3493
codinBabe <teboola2@gmail.com>
2024-08-09 13:47:19 +0100

    Completed:task 9-11

--- 0x02-ES6_classes/7-airport.js
+++ 0x02-ES6_classes/7-airport.js
@@ -1,24 +1,24 @@
 export default class Airport {
-    constructor(name, code) {
-        this._name = name;
-        this._code = code;
-    }
+  constructor(name, code) {
+    this._name = name;
+    this._code = code;
+  }
 
-    get name() {
-        return this._name;
-    }
-    set name(value) {
-        this._name = value;
-    }
+  get name() {
+    return this._name;
+  }
+  set name(value) {
+    this._name = value;
+  }
 
-    get code() {
-        return this._code;
-    }
-    set code(value) {
-        this._code = value;
-    }
+  get code() {
+    return this._code;
+  }
+  set code(value) {
+    this._code = value;
+  }
 
-    toString() {
-        return `[object ${this._code}]`;
-    }
+  toString() {
+    return `[object ${this._code}]`;
+  }
 }
--- 0x02-ES6_classes/8-hbtn_class.js
+++ 0x02-ES6_classes/8-hbtn_class.js
@@ -1,41 +1,29 @@
 export default class HolbertonClass {
-    constructor(size, location) {
-        if (typeof size !== 'number') {
-        throw TypeError('Size must be a number');
-        }
-        if (typeof location !== 'string') {
-        throw TypeError('Location must be a string');
-        }
-        this._size = size;
-        this._location = location;
+  constructor(size, location) {
+    this._size = size;
+    this._location = location;
+  }
+
+  get size() {
+    return this._size;
+  }
+  set size(value) {
+    this._size = value;
+  }
+  get location() {
+    return this._location;
+  }
+  set location(value) {
+    this._location = value;
+  }
+
+  [Symbol.toPrimitive](hint) {
+    if (hint === 'number') {
+      return this._size;
     }
-    
-    get size() {
-        return this._size;
+    if (hint === 'string') {
+      return this._location;
     }
-    set size(value) {
-        if (typeof value !== 'number') {
-        throw TypeError('Size must be a number');
-        }
-        this._size = value;
-    }
-    get location() {
-        return this._location;
-    }
-    set location(value) {
-        if (typeof value !== 'string') {
-        throw TypeError('Location must be a string');
-        }
-        this._location = value;
-    }
-    
-    [Symbol.toPrimitive](hint) {
-        if (hint === 'number') {
-        return this._size;
-        }
-        if (hint === 'string') {
-        return this._location;
-        }
-        return null;
-    }
-    }
+    return null;
+  }
+}
